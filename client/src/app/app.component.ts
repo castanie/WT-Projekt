067c462d7266adf4413d1631c16ce35783fc6b4c
Daniel Egger <dan.egg@outlook.com>
2022-01-13 11:29:15 +0100

    Add client layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pizza Margherita';
+  desc = 'Everyone knows and loves it – pizza margherita is a universally praised pizza for a reason. Originating in Naples, the margherita pizza has an interesting history supposedly rooted in a visit by Queen Margherita to Naples. The iconic pizza margherita is also known for representing the colours of the Italian flag: red tomato sauce, white mozzarella, and green basil. The combination of these ingredients creates a delicious pizza which has withstood the test of time';
+  price = '6.80';
 }
