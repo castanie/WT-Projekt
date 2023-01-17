e4becbaa15b33efb1bf076388e78c0afcd9fe119
Daniel Egger <dan.egg@outlook.com>
2023-01-17 20:23:35 +0100

    Add basic 'GET' method to 'film.service'

--- client/src/app/film.service.ts
+++ client/src/app/film.service.ts
@@ -1,8 +1,26 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, Observable, of } from "rxjs";
+import { Film } from "./film.model";
 
 @Injectable({
     providedIn: "root",
 })
 export class FilmService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
+
+    getFilms(): Observable<Film[]> {
+        return this.http
+            .get<Film[]>("url")
+            .pipe(catchError(this.handleError<Film[]>([])));
+    }
+
+    private handleError<T>(result?: T) {
+        return (error: any): Observable<T> => {
+            // ...:
+            console.error(error);
+            // Let the app keep running by returning an empty result:
+            return of(result as T);
+        };
+    }
 }
