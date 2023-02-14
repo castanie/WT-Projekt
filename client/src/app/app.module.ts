51ba018a3c922a06c545254e9d10ef732b994544
Daniel Egger <dan.egg@outlook.com>
2023-02-14 23:10:05 +0100

    Fix 'film-page' sub-components and enable anchors

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FilmPageModule } from "./film-page/film-page.module";
 import { HomePageModule } from "./home-page/home-page.module";
 
 @NgModule({
@@ -11,6 +12,7 @@ import { HomePageModule } from "./home-page/home-page.module";
     imports: [
         AppRoutingModule,
         BrowserModule,
+        FilmPageModule,
         HomePageModule,
         HttpClientModule,
     ],
